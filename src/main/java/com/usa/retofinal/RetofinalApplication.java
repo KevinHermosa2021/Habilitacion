afa691eec7087ee67c68789804fc719dbbe3a89a
KevinHermosa <89666385+KevinHermosa@users.noreply.github.com>
2021-12-22 23:37:32 -0500

    Update product controller and apliation.java

--- src/main/java/com/usa/retofinal/RetofinalApplication.java
+++ src/main/java/com/usa/retofinal/RetofinalApplication.java
@@ -1,13 +1,22 @@
 package com.usa.retofinal;
 
+import com.usa.retofinal.repository.crudrepository.ProductCrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RetofinalApplication {
+public class RetofinalApplication implements CommandLineRunner {
+    @Autowired
+    private ProductCrudRepository ProductCrudRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(RetofinalApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        ProductCrudRepository.deleteAll();
+    }
 }
